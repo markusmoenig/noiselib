886e287d7a3bdee553549ba26be29bb6e6f9cf2e
Markus Moenig <markus@moenig.io>
2024-04-04 09:05:02 +0700

    3D and 4D Fractal Mul versions

--- src/fractal.rs
+++ src/fractal.rs
@@ -54,3 +54,66 @@ pub fn fractal_noise_mul_2d<F: Fn(&mut UniformRandomGen, f32, f32, u32) -> f32>(
 
     rnd_val
 }
+
+#[allow(clippy::too_many_arguments)]
+pub fn fractal_noise_mul_3d<F: Fn(&mut UniformRandomGen, f32, f32, f32, u32) -> f32>(
+    rng: &mut UniformRandomGen,
+    x: f32,
+    y: f32,
+    z: f32,
+    noise_func: F,
+    octaves: i32,
+    freq_falloff: f32,
+    lacunarity: f32,
+    offset: f32,
+    seed: u32,
+) -> f32 {
+    let mut rnd_val = 1.0;
+    let mut power = 1.0;
+    let mut x = x;
+    let mut y = y;
+    let mut z = z;
+
+    for _i in 0..octaves {
+        rnd_val *= (noise_func(rng, x, y, z, seed) + offset) * power;
+        power *= freq_falloff;
+        x *= lacunarity;
+        y *= lacunarity;
+        z *= lacunarity;
+    }
+
+    rnd_val
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn fractal_noise_mul_4d<F: Fn(&mut UniformRandomGen, f32, f32, f32, f32, u32) -> f32>(
+    rng: &mut UniformRandomGen,
+    x: f32,
+    y: f32,
+    z: f32,
+    t: f32,
+    noise_func: F,
+    octaves: i32,
+    freq_falloff: f32,
+    lacunarity: f32,
+    offset: f32,
+    seed: u32,
+) -> f32 {
+    let mut rnd_val = 1.0;
+    let mut power = 1.0;
+    let mut x = x;
+    let mut y = y;
+    let mut z = z;
+    let mut t = t;
+
+    for _i in 0..octaves {
+        rnd_val *= (noise_func(rng, x, y, z, t, seed) + offset) * power;
+        power *= freq_falloff;
+        x *= lacunarity;
+        y *= lacunarity;
+        z *= lacunarity;
+        t *= lacunarity;
+    }
+
+    rnd_val
+}
